6b353656e8e6ea741396903f6d014a64e834b578
ankit18-259 <ankit18_259@aec.ac.in>
2020-07-13 16:31:39 +0530

    Fixes #1

---
+++ public/js/indexScript.js
@@ -0,0 +1,50 @@
+var votesForCandidate1 = 0;
+var votesForCandidate2 = 0;
+var votesForCandidate3 = 0;
+var votesForCandidate4 = 0;
+var votesForCandidate5 = 0;
+var votesForCandidate6 = 0;
+var votesForCandidate7 = 0;
+var votesForCandidate8 = 0;
+
+function abcd(id)
+{
+    
+    if(id=="candidate1")
+    {
+        votesForCandidate1++;
+    }
+    if(id=="candidate1")
+    {
+        votesForCandidate2++;
+    }
+    if(id=="candidate1")
+    {
+        votesForCandidate3++;
+    }
+    if(id=="candidate1")
+    {
+        votesForCandidate4++;
+    }
+    if(id=="candidate1")
+    {
+        votesForCandidate5++;
+    }
+    if(id=="candidate1")
+    {
+        votesForCandidate6++;
+    }
+    if(id=="candidate1")
+    {
+        votesForCandidate7++;
+    }
+    if(id=="candidate1")
+    {
+        votesForCandidate8++;
+    }
+    alert("Done");
+}
+function submit()
+{
+    alert("Submitted")
+}
